5f0e44304a1f8a5a2da15ec763301800e45f948b
痞子鱼 <siseboy@users.noreply.github.com>
2017-01-10 16:39:14 +0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
-# junichi
-Typecho Theme
+###Junichi，Typecho主题
+
+Junichi 是 Typecho 的一套主题模板，Junichi 这个主题的名字由来，是因为博主起名实在没天分，所以就用我宝贝儿子的英文名来命名。
+
+主题从2016年11月14日开始制作，现在只是有形无神，正在进一步优化代码和适配功能。
+
+
+下载地址：
+
+https://github.com/siseboy/junichi
+
+主题预览为本站：
+
+https://www.uefeng.com
+
+####主题亮点
+
+ - 无jQuery，无前端框架，轻量级
+ - InstantClick实现PJAX无刷新操作
+ - Prismjs代码高亮集成
+ - 响应式设计，手机浏览效果更棒
+ - 支持图片CDN
+ - 支持社交按钮，但无社交分享
+
+####主题激活
+
+下载后，重命名文件夹为junichi，然后放到typecho里的theme目录内，进入typecho后台，外观，激活即可。
+
+####主题设置
+
+进入主题外观设置
+
+依次是标语，开启Pjax和的社交链接
+
+然后是图片CDN，推荐使用七牛云镜像
+
+
---
+++ page-links.php
@@ -0,0 +1,32 @@
+<?php
+    /**
+    * links
+    *
+    * @package custom
+    */
+    $this->need('header.php'); ?>
+
+	<div class="mid-col">
+		<div class="mid-col-container">
+			<div id="content" class="inner">
+				<article class="post post-archives">
+					<h1 class="title"><?php $this->title() ?></h1>
+					<div class="entry-content">	
+	
+                        <ul class="links">
+					       <li><i class="iconfont icon-https"></i><a href="https://itlu.org/" target="_blank">挨踢路</a><span class="more">（混迹独立博客圈8年的老博主）</span></li>
+				        </ul>
+                       
+                        <ul>
+					       <li><a href="http://www.cqnv.com/" target="_blank">重庆游记</a><span class="more">（深藏不露的博士生一个）</span></li>			<li><a href="http://jixufengyu.com/" target="_blank">几许风雨</a><span class="more"></span></li>
+                            <li><a href="http://www.4zen.top/" target="_blank">禅心</a><span class="more">（普通男青年,有点伪文艺）</span></li>      
+					       
+				        </ul>
+
+					</div>
+				</article>
+			</div>
+		</div>
+
+
+<?php $this->need('footer.php'); ?>
